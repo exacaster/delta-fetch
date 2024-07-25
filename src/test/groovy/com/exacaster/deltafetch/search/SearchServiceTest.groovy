eafce3d1bbd5300d363cc61061321e8882ff8c46
edvaone <edvaone@gmail.com>
2024-07-25 15:12:55 +0300

    Convert DECIMAL type (#53)
    Author: edvardas <edvardas@exacaster.com>

--- src/test/groovy/com/exacaster/deltafetch/search/SearchServiceTest.groovy
+++ src/test/groovy/com/exacaster/deltafetch/search/SearchServiceTest.groovy
@@ -30,4 +30,28 @@ class SearchServiceTest extends Specification {
         then: "returns empty"
         result.isEmpty()
     }
+    def worksWithDifferentTypes() {
+        given:
+        def cache = Mock(SyncCache) {
+            get(*_) >> Optional.empty()
+        }
+        def conf = new Configuration()
+        def statsReader = new DeltaMetaReader(conf, cache)
+        def svc = new SearchService(statsReader, new Configuration())
+        def path = getClass().getResource("/test_data_types").toString()
+
+        when:
+        def result = svc.find(path, [new ColumnValueFilter("user_id", "555")], true, 1).findFirst()
+
+        then:
+        result.isPresent()
+        result.get().getValue().get("trait_string") == "ACTIVE"
+        result.get().getValue().get("trait_decimal18_3") == 5.555
+        result.get().getValue().get("trait_decimal21_3") == 55.123
+        result.get().getValue().get("trait_double") == 12345.678
+        result.get().getValue().get("trait_float") == 12345.678F
+        result.get().getValue().get("trait_int") == 12345
+        result.get().getValue().get("trait_bigint") == 123456789012345
+        result.get().getValue().get("trait_boolean") == true
+    }
 }
